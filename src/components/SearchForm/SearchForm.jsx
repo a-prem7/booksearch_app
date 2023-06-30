9e0bf99ccae6bae6920c8edbd0262970abed3219
amal-prem <aldamal.prem@gmail.com>
2023-06-29 21:10:47 -0400

    adds navbar functionality

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react';
+import {FaSearch} from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import "./SearchForm.css";
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
-  )
-}
-
-export default SearchForm
+        <div className='search-form'>
+          <div className='container'>
+            <div className='search-form-content'>
+              <form className='search-form' onSubmit={handleSubmit}>
+                <div className='search-form-elem flex flex-sb bg-white'>
+                  <input type = "text" className='form-control' placeholder='The Lost World ...' ref = {searchText} />
+                  <button type = "submit" className='flex flex-c' onClick={handleSubmit}>
+                    <FaSearch className='text-purple' size = {32} />
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    
+    export default SearchForm
