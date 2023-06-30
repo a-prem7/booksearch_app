654dadc808545d7987931f578b2dc001574c3b3c
amal-prem <aldamal.prem@gmail.com>
2023-06-29 21:25:56 -0400

    adds loader functionality

---
+++ src/context.js
@@ -0,0 +1,10 @@
+import React, {useState, useContext, useEffect} from 'react';
+import { useCallback } from 'react';
+const URL = "http://openlibrary.org/search.json?title=";
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    const [searchTerm, setSearchTerm] = useState("the lost world");
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [resultTitle, setResultTitle] = useState("");
